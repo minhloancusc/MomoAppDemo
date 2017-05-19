305684bd9270ab870819a2bd2f43bb8fa100548b
cuscedu <cuscedu@mac-gv.local>
2017-05-19 15:49:44 +0700

    comment branch 2

--- MyTest/ViewController.swift
+++ MyTest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Version 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
